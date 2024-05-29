4a2b85b8589cc93b56fe07a40a77875b159e0212
Moustafa <moustafaadel097@gmail.com>
2024-05-29 19:48:23 +0300

    index

--- backend/index.js
+++ backend/index.js
@@ -33,25 +33,25 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve("../");
-  app.use(express.static(path.join(__dirname + "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+// const __dirname = path.resolve("../");
+// app.use(express.static(path.join(__dirname + "/frontend/build")));
 
-  app.use("/admin", AdminRoutes);
-  app.use("/user", UserRoutes);
-  app.get("/", (req, res) => {
-    res.send("server is ready");
-  });
-  app.use(notFound);
-  app.use(errorHandler);
-
-  app.get("*", (req, res, next) => {
-    return res.sendFile(
-      path.join(__dirname, "frontend", "build", "index.html")
-    );
-  });
-}
+// app.get("*", (req, res, next) => {
+//   return res.sendFile(
+//     path.join(__dirname, "frontend", "build", "index.html")
+//   );
+// });
+// }
 
 connectDB();
 mongoose.set("strictQuery", false);
 app.listen(PORT, () => console.log(`started on ${PORT}`));
+
+app.use("/admin", AdminRoutes);
+app.use("/user", UserRoutes);
+app.get("/", (req, res) => {
+  res.send("server is ready");
+});
+app.use(notFound);
+app.use(errorHandler);
